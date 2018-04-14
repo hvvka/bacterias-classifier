7e48476485e609447a75010e994183d934e23918
Hanna Grodzicka <hania.grodzicka@gmail.com>
2018-04-14 16:09:42 +0200

    Add bacteria classifiers

--- db/src/main/java/com/hania/SqliteConnection.java
+++ db/src/main/java/com/hania/SqliteConnection.java
@@ -1,15 +1,16 @@
 package com.hania;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * @author <a href="mailto:226154@student.pwr.edu.pl">Hanna Grodzicka</a>
  */
-public interface SqliteConnection {
+public interface SqliteConnection extends AutoCloseable  {
 
     Connection connect();
 
     Connection getConnection();
 
-    void disconnect();
+    void close() throws SQLException;
 }
---
+++ src/main/java/com/hania/Bacterias.java
@@ -0,0 +1,20 @@
+package com.hania;
+
+import com.hania.examined.Examined;
+import com.hania.knn.NearestNeighbour;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:226154@student.pwr.edu.pl">Hanna Grodzicka</a>
+ */
+public class Bacterias {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Bacterias.class);
+
+    public static void main(String[] args) {
+        NearestNeighbour nearestNeighbour = new NearestNeighbour("328734");
+        Examined examined = nearestNeighbour.classify();
+        LOG.info("Class of {}: {}", examined.getGenotype(), examined.getClassification());
+    }
+}
